f8ffac4da5225da1d373b3ffb6a1f7b207572408
walia201 <75194452+walia201@users.noreply.github.com>
2022-05-01 11:38:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> UPSC </h1>
+      <h2>The Union Public Service Commission, commonly abbreviated as UPSC, is India's premier central recruitment agency for recruitment of all the Group 'A' officers under Government of India. It is responsible for appointments and examinations for all of the Group 'A' posts of all the union government establishments which also includes all of the central public sector units and all of the central autonomous bodies. While Department of Personnel and Training is the central personnel agency in India.
+
+The agency's charter is granted by Part XIV of the Constitution of India, titled as Services Under the Union and the States. The commission is mandated by the Constitution for appointments to the services of the Union and All India Services. It is also required to be consulted by the Government in matters relating to the appointment, transfer, promotion and disciplinary matters. The commission reports directly to the President and can advise the Government through him. Although, such advice is not binding on the Government. Being a constitutional authority, UPSC is amongst the few institutions which function with both autonomy and freedom, along with the country’s higher judiciary and lately the Election Commission.[4]
+
+The commission is headquartered at Dholpur House, in New Delhi and functions through its own secretariat. Dr. Manoj Soni has been the Chairman of UPSC since 5 APRIL 2022.
+
+Established on 1 October 1926 as Public Service Commission, it was later reconstituted as Federal Public Service Commission by the Government of India Act 1935; only to be renamed as today's Union Public Service Commission after the independence.</h2>
+    </div>
+  );
+}
